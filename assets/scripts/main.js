7fbd6ad5a8417fa7976169ab759ab8d4c93afd2e
rafvi <raffi.ssj@gmail.com>
2021-10-10 19:30:17 +0200

    update

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+/*
+const arrayTags = [
+    'adware ', 'android ', 'autostart ', 'backup ', 'bios ', 'blue_screen ', 'Browsers ', 'błędy ', 
+    'c_sharp ', 'CD ', 'CDBurnerXP ', 'Chrome ', 'cmd ', 'diagnostyka_pamięci ', 'diskpart ', 'DISM', 'dll ', 'drukarki ', 
+    'DVD ', 'excel ', 'exif ', 'Firefox ', 'gif ', 'HDD_Raw_Copy_Tool ', 'Informacje_o_systemie ', 'instalacja_Windows_8 ',
+    'ISO ', 'klonowanie_karty_SD ', 'konto_użytkownika ', 'kopia_zapasowa ', 'LibreOffice ', 'macierz ', 'Memory_Diagnostic ', 
+    'Menadżer_urządzeń ', 'metadane ', 'Mozilla ', 'msconfig ', 'msinfo32 ', 'nagraj_obraz_ISO ', 'naprawa_systemu_Windows ', 
+    'narzędzie_konfiguracji_systemu ', 'nieznane_urządzenie ', 'OCX ', 'odzyskanie_sesji ', 'office ', 'OLE', 'pamięć_ram ', 'pasek_zadań ', 
+    'path ', 'pdf ', 'przywracaniesystemu ', 'RAM ', 'ramdysk ', 'regedit ', 'regsvr32 ', 'root ', 'sfc ', 'siec_i_internet ', 'sterowniki ', 
+    'usb ', 'web_browser ', 'Windows ', 'Windows_8 ', 'Windows_XP ', 'www ', 'youtube '
+];
+
+const tags = document.querySelector('.widget-tags');
+
+arrayTags.forEach(element => {
+    let tag = document.createElement('div');
+    tag.setAttribute('id', 'tag');
+    tags.appendChild(tag);
+
+    let tag_a = document.createElement('a');
+    tag_a.innerHTML = element;
+    tag.appendChild(tag_a).href = "/"
+})
+*/
